144be1f90e1fe75ffd73a5eca67f0c011bc38554
mdimon313 <imon26362@gmail.com>
2022-08-17 01:31:55 +0600

    auth firebase

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useAuthContext } from "../context/AuthContext";
 
 function Dashboard() {
   window.document.title = "Dashboard";
+  const { signout } = useAuthContext();
+
+  function handleLogout() {
+    signout();
+  }
   return (
     <div className="flex items-center justify-center min-h-screen w-screen">
       Dashboard
+      <button onClick={handleLogout}>signout</button>
     </div>
   );
 }
